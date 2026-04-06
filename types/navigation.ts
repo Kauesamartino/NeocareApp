0b98c0f13d00704dbfb5f8960791b5558a34253d
Kauesamartino <kaue_samartino@hotmail.com>
2026-04-06 01:55:36 -0300

    feat: add RootStackParamList and typed navigation helpers

---
+++ types/navigation.ts
@@ -0,0 +1,21 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RouteProp } from '@react-navigation/native';
+import { DadosPessoais, Credenciais } from './cadastro';
+
+export type RootStackParamList = {
+  Login: undefined;
+  Cadastro: undefined;
+  CadastroDadosPessoais: undefined;
+  CadastroCredenciais: { dadosPessoais: DadosPessoais };
+  CadastroEndereco: {
+    dadosPessoais: DadosPessoais;
+    credenciais: Credenciais;
+  };
+  Home: undefined;
+};
+
+export type AppNavigationProp<T extends keyof RootStackParamList> =
+  NativeStackNavigationProp<RootStackParamList, T>;
+
+export type AppRouteProp<T extends keyof RootStackParamList> =
+  RouteProp<RootStackParamList, T>;
